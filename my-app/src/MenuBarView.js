59b5f5cbb4ba261f6fabedaf1c1ea22524556208
Frederik Riedel <frederik.riedel@frogg.io>
2018-06-15 15:54:09 -0700

    added textfield popover when pressing "open"

--- my-app/src/MenuBarView.js
+++ my-app/src/MenuBarView.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
+import PublishIcon from '@material-ui/icons/Publish';
 import FolderIcon from '@material-ui/icons/Folder';
-import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
 import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Typography from '@material-ui/core/Typography';
+import IconArrowForward from '@material-ui/icons/ArrowForward';
+import Popover from '@material-ui/core/Popover';
 //import Grid from '@material-ui/core/Grid';
 
 const BG = "CadetBlue";
+
+
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
 
 class MenuBarView extends Component {
   constructor(props) {
@@ -15,41 +37,96 @@ class MenuBarView extends Component {
     this.onSave = this.onSave.bind(this);
     this.onLoad = this.onLoad.bind(this);
     this.contentId = 0;
+    this.onOpen = this.onOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.state = {
+            anchorEl: null,
+        }
   }
 
-  onSave(){
+  onSave() {
     this.props.onSave(this.contentId, false);
   }
 
-  onPublish(){
+  onPublish() {
     this.props.onSave(this.contentId, true);
   }
 
-  onLoad(){
+  onOpen(event) {
+    this.setState({
+      anchorEl: event.currentTarget,
+    });
+  }
+
+  handleClose() {
+    this.setState({
+      anchorEl: null,
+    });
+  };
+
+  onLoad() {
     this.props.onLoad(this.contentId);
+    this.handleClose()
   }
 
   onChange(event) { 
     this.contentId = event.target.value;
   }
 
+  onClosePopup() {
+    
+  };
+
   render() {
     return (
     	<div>
       <Toolbar style={{backgroundColor: BG}}>
-      <Button color="inherit" onClick={this.onLoad}>
-          <FolderIcon />
+      <Button color="inherit" onClick={this.onOpen} color="white">
+        <FolderIcon />
           Open
       </Button>
-    	<Button color="inherit" onClick={this.onSave}>
+      <Popover
+          open={Boolean(this.state.anchorEl)}
+          anchorEl={this.state.anchorEl}
+          onClose={this.onClosePopup}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+        >
+          
+        <Input
+          placeholder="Content ID (Link)"
+          inputProps={{
+            'aria-label': 'Description',
+          }}
+          endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="Open Content"
+                  onClick={this.onLoad}
+                  onClose={this.handleClose}
+                >
+                <IconArrowForward />
+                </IconButton>
+              </InputAdornment>
+            }
+        />
+
+      </Popover>
+    	<Button color="inherit" onClick={this.onSave} color="white">
         <SaveIcon />
 	        Save Draft
-	      </Button>
-      <Button  color="inherit" onClick={this.onPublish}>
-        <SaveIcon />
+	    </Button>
+      <Button  color="inherit" onClick={this.onPublish} color="white">
+        <PublishIcon />
           Publish on Oasys
         </Button>
-        </Toolbar>
+      </Toolbar>
 	    </div>
 	)
   }
