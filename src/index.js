014ba019b46307c3eb6f71da0e7194e338a3822b
Frederik Riedel <frederik.riedel@frogg.io>
2018-06-20 13:41:00 -0700

    removed legacy app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import {Component} from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 
