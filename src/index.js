ab2c4062c0991e7bc6790e24028195667413b692
Frederik Riedel <frederik.riedel@frogg.io>
2018-06-22 14:19:11 -0700

    personal account page is now shown when clicking on MY ACCOUNT fixing #42

--- src/index.js
+++ src/index.js
@@ -28,9 +28,9 @@ class Index extends Component {
                             <Route path="/learn" component={ContentSelection}/>
                             <Route path="/create" component={Editor} />
 
-                            <Route path="user/:username/:contentname" component={ContentViewTest}/>
-                            <Route path="user/:username" component={UserPage}/>
-                            <Route path="user/" component={UserPage}/>
+                            <Route path="/user/:username/:contentname" component={ContentViewTest}/>
+                            <Route path="/user/:username" component={UserPage}/>
+                            <Route path="/user" component={MyAccountPage}/>
 
 
                             <Route component={NotFoundPage}/>
@@ -48,9 +48,16 @@ const UserPage = ({ match }) => (
         <h3>Username: {match.params.username}</h3>
     </div>
 );
+
+const MyAccountPage = ({ match }) => (
+    <div>
+        <h3>This is your personal user account page. You see all your content here. In the future. Right now, this is just a demo text.</h3>
+    </div>
+);
+
 const ContentViewTest = ({ match }) => (
     <div>
-        <h3>ContentName: {match.params.contentname}</h3>
+        <h3>ContentName: {match.params.contentname} of user: {match.params.username}</h3>
     </div>
 );
 
