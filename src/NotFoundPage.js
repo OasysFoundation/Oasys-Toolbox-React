02415f84a7fcbf05ead687d80ef813bbc57b5a3a
Frederik Riedel <frederik.riedel@frogg.io>
2018-06-22 14:53:03 -0700

    made 404 page look nice

--- src/NotFoundPage.js
+++ src/NotFoundPage.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from "styled-components"
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
 
 
 function NotFoundPage() {
     return (
-        <h1>
-            Wrong Place my Friend. We're sorry. One of us made a mistake (or our servers crashed :-( )
-        </h1>
+		<center>
+	      <Paper style={{margin: 36, padding: 16, maxWidth: '400px', textAlign:'left'}} elevation={4}>
+	        <Typography variant="headline" component="h3">
+	          The page you’re looking for can’t be found.
+	        </Typography>
+	        <Typography component="p">
+	          Please contact us at info@joinoasys.org if you think that this webpage should exist.
+	        </Typography>
+	      </Paper>
+	    </center>
     )
 }
 
