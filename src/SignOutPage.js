975a6ec55bfdb0c2d58f7f9111fd975621d31eed
Robbie Zuazua <rzuazua@utexas.edu>
2018-07-01 22:06:24 -0500

    snack bar when user is not signed in (signing out or first time logged in)

--- src/SignOutPage.js
+++ src/SignOutPage.js
@@ -8,78 +8,19 @@ import CloseIcon from '@material-ui/icons/Close';
 import { withStyles } from '@material-ui/core/styles';
 
 
-const styles = theme => ({
-  close: {
-    width: theme.spacing.unit * 4,
-    height: theme.spacing.unit * 4,
-  },
-});
-
-
-
 class SignOutPage extends React.Component {
 
-  state = {
-    open: false,
-  };
 
-  handleClick = () => {
-    this.setState({ open: true });
-    auth.doSignOut();
-  };
-
-  handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    this.setState({ open: false });
-  };
 
    render() {
-        const { classes } = this.props;
-
     return (
-      <div>
-  <Button
-    type="button"
-    color="inherit"
-    onClick={this.handleClick}
-  >Sign Out</Button>
-  <Snackbar
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
-          }}
-          open={this.state.open}
-          autoHideDuration={6000}
-          ContentProps={{
-            'aria-describedby': 'message-id',
-          }}
-          message={<span id="message-id">Signed Out</span>}
-          action={[
-            <Button key="undo" color="secondary" size="small" onClick={this.handleClose}>
-              UNDO
-            </Button>,
-            <IconButton
-              key="close"
-              aria-label="Close"
-              color="inherit"
-              className={classes.close}
-              onClick={this.handleClose}
-            >
-              <CloseIcon />
-            </IconButton>,
-          ]}
-        />
-</div>
-
-  )
-}
+      <Button
+        type="button"
+        color="inherit"
+        onClick={auth.doSignOut}
+      >Sign Out</Button>
+    )
+  }
 }
 
-SignOutPage.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(SignOutPage);   
+export default SignOutPage;   
