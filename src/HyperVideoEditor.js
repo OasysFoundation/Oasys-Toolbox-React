6cabda675d168060b87605659f35d4038521955f
Daniel Renz <daniel.l.renz@gmail.com>
2018-07-04 21:19:49 +0200

    temp hypervideo commit, almost done, but currently broken. need to propate state correctyle from plyrwrapper to hypervidoeeditor

--- src/HyperVideoEditor.js
+++ src/HyperVideoEditor.js
@@ -15,142 +15,7 @@ const defaultQuiz = {
   "answers": [{"option": "", "correct": false}]
 };
 
-class HyperVideoEditor extends Component {
-
-	constructor(props) {
-	    super(props);
-	    this.state = {
-	    	currentTime: 0,
-	    	currentQuiz: null,
-	    	quizzes: this.props.value.quizzes,
-	    	videoURL: this.props.value.videoURL
-	    }
-	}
-
-	jump() {
-    	plyr.currentTime = 10;
-    	plyr.clickToPlay = false;
-    	plyr.keyboard = { focused: false, global: false };
-    	window.plyr = plyr;
-	}
-
-	onChange(content) {
-		console.log(content);
-	}
-
-	addNewQuizAtCurrentTime() {
-		plyr.pause();
-		
-		plyr.toggleControls(false);
-
-
-		let newQuiz = defaultQuiz;
-		newQuiz.time = plyr.currentTime;
-		let quizzes = this.state.quizzes;
-		quizzes.push(newQuiz);
-
-		this.setState({
-			quizzes: quizzes
-		});
-
-		this.refreshCurrentQuiz();
-	}
-
-	onChange(content) {
-		this.setState({
-			quizzes: content
-		}, function() {
-			this.refreshCurrentQuiz();
-			this.notifyDelegate();
-		})
-	}
-
-	notifyDelegate() {
-		this.props.onChange({
-			"videoURL": this.state.videoURL,
-			"quizzes": this.state.quizzes
-		});
-	}
-
-	didChangeVideoURL(textfield) {
-		const url = textfield.target.value;
-		console.log(url);
-		if (this.isValidYouTubeUrl(url)) {
-			this.setState({
-				videoURL: '' + url
-			}, function() {
-				this.notifyDelegate();
-			});
-		}
-	}
-
-	isValidYouTubeUrl(url)
-	{
-        if (url != undefined || url != '') {
-            var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
-            var match = url.match(regExp);
-            if (match && match[2].length == 11) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-	}
-
-	render() {
- 	   return (
- 	   	<div id='hyperVideoEditor'>
- 	   		<TextField
-              id="name"
-              placeholder="YouTube URL"
-              margin="normal"
-              style={{width:'100%', 'margin-bottom': '20px'}}
-              onChange={this.didChangeVideoURL.bind(this)}
-              helperText="Wrong URL format"
-            />
- 	   		<center>
- 	   		<Plyr
-		      type="youtube" // or "vimeo"
-		      videoId={this.state.videoURL}
-		      ref="video"
-		      clickToPlay= {false}
-		      clickToPause= {false}
-		      controls= {['play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen']}
-		    />
-
-		    <Button variant="raised" onClick={this.addNewQuizAtCurrentTime.bind(this)} style={{color: 'black'}} >
-	          Insert Quiz at {Math.round(this.state.currentTime)} seconds
-	      	</Button>
- 	   		</center>
- 	   		<HorizontalSlidePicker quizzes={this.state.quizzes} onChange={this.onChange.bind(this)} />
- 	   	</div>
- 	   	);
-	}
-
-	componentDidMount() {
-
-		const player = this.refs.video;
-		plyr = player.player;
-
-		const elem = document.createElement('div');
-		elem.innerHTML = "";
-		elem.id = "overlay-container"
-		elem.setAttribute('style',  "position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; z-index:1000; pointer-events: all;");
-		plyr.elements.container.children[0].append(elem);
-
-		
-		plyr.on("timeupdate", event => {
-			const instance = event.detail.plyr;
-			
-			this.setState({
-				currentTime: instance.currentTime
-			});
-
-			this.refreshCurrentQuiz();
-			
-		})
-	}
+class PlyrWrapper extends Component {
 
 	onCompletedQuiz() {
 		plyr.play();
@@ -190,6 +55,163 @@ class HyperVideoEditor extends Component {
 				});
 			}
 	}
+
+
+	jump() {
+    	plyr.currentTime = 10;
+    	plyr.clickToPlay = false;
+    	plyr.keyboard = { focused: false, global: false };
+    	window.plyr = plyr;
+	}
+
+	addNewQuizAtCurrentTime() {
+		plyr.pause();
+		
+		plyr.toggleControls(false);
+
+
+		let newQuiz = defaultQuiz;
+		newQuiz.time = plyr.currentTime;
+		let quizzes = this.state.quizzes;
+		quizzes.push(newQuiz);
+
+		this.setState({
+			quizzes: quizzes
+		});
+
+		this.refreshCurrentQuiz();
+	}
+
+	onChange(content) {
+		this.setState({
+			quizzes: content
+		}, function() {
+			this.refreshCurrentQuiz();
+			this.notifyDelegate();
+		})
+	}
+
+	notifyDelegate() {
+		this.props.onChange({
+			"videoURL": this.state.videoURL,
+			"quizzes": this.state.quizzes
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<center>
+				<Plyr
+			      type="youtube" // or "vimeo"
+			      videoId={this.state.videoURL}
+			      ref="video"
+			      clickToPlay= {false}
+			      clickToPause= {false}
+			      controls= {['play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen']}
+			    />
+
+			    <Button variant="raised" onClick={this.addNewQuizAtCurrentTime.bind(this)} style={{color: 'black'}} >
+		          Insert Quiz at {Math.round(this.state.currentTime)} seconds
+		      	</Button>
+	 	   		</center>
+	 	   		<HorizontalSlidePicker quizzes={this.state.quizzes} onChange={this.onChange.bind(this)} />
+		 	</div>
+		 )
+	}
+	
+
+}
+
+class HyperVideoEditor extends Component {
+
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	    	currentTime: 0,
+	    	currentQuiz: null,
+	    	quizzes: this.props.value.quizzes,
+	    	videoURL: this.props.value.videoURL
+	    }
+	}
+
+	notifyDelegate() {
+		this.props.onChange({
+			"videoURL": this.state.videoURL,
+			"quizzes": this.state.quizzes
+		});
+	}
+
+	didChangeVideoURL(textfield) {
+		const url = textfield.target.value;
+		console.log(url);
+		if (this.isValidYouTubeUrl(url)) {
+			this.setState({
+				videoURL: '' + url
+			}, function() {
+				this.notifyDelegate();
+			});
+		}
+	}
+
+	isValidYouTubeUrl(url)
+	{
+        if (url != undefined || url != '') {
+            var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
+            var match = url.match(regExp);
+            if (match && match[2].length == 11) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+	}
+
+	render() {
+ 	   return (
+ 	   	<div id='hyperVideoEditor'>
+ 	   	{this.state.videoURL === null ? 
+ 	   		<TextField
+              id="name"
+              placeholder="YouTube URL"
+              margin="normal"
+              style={{width:'100%', 'margin-bottom': '20px'}}
+              onChange={this.didChangeVideoURL.bind(this)}
+              helperText="Wrong URL format"
+            />
+           :
+           <PlyrWrapper/>
+ 	   	}
+ 	   		
+ 	   	</div>
+ 	   	);
+	}
+
+	componentDidMount() {
+
+		const player = this.refs.video;
+		plyr = player.player;
+
+		const elem = document.createElement('div');
+		elem.innerHTML = "";
+		elem.id = "overlay-container"
+		elem.setAttribute('style',  "position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; z-index:1000; pointer-events: all;");
+		plyr.elements.container.children[0].append(elem);
+
+		
+		plyr.on("timeupdate", event => {
+			const instance = event.detail.plyr;
+			
+			this.setState({
+				currentTime: instance.currentTime
+			});
+
+			this.refreshCurrentQuiz();
+			
+		})
+	}
+
 }
 
 export default HyperVideoEditor;
