6e82584c17dd6c8219fe3ab901bef144ed5df964
Daniel Renz <daniel.l.renz@gmail.com>
2018-07-10 13:58:22 +0200

    prepare functions to generate fake data

--- src/DataView.js
+++ src/DataView.js
@@ -1,14 +1,51 @@
 import React, {Component} from 'react';
 import {Line, Bar} from 'react-chartjs-2';
 
-const allContentsForUser = {
-    
+const allContentsForUser = [];
+
+generateFakeSlideTimes(n) {
+    a = [];
+    let i=0;
+    while(true) {
+        a.push({i: i, t: Math.random()*1000*60)}); // max 1 minute per slide
+        if (i==n) { break; }
+        else if (Math.random() > 0.9 && i > 0) { i--; }
+        else {i++; }
+    }
+    return a;
+}
+
+
+generateFakeQuizAnswers(n) {
+    a = [];
+    for (let i=0; i<n; i++) {
+        if (Math.random() > 0.7) {
+            a.push(false);
+        } else {
+            a.push(true);
+        }
+    }
+}
+
+generateFakeData(){
+    for (let =0;i<5;i++) {
+        let nSlides = 20 - Math.round(Math.random() * 10);
+        let nQuiz = 4 - Math.round(Math.random() * 3);
+        let startTime = new Date() - Math.random()*1000*60*60*24*30 // startTime in interval [now-30 days, now]
+        let endTime = startTime + Math.random()*1000*60*60 
+        let slideTimings = generateFakeSlideTimes(nSlides)
+        let quizAnswers = generateFakeQuizAnswers(nQuiz)
+        content = {startTime: startTime, endTime: endTime, slideTimings: slideTimings, quizAnswers: quizAnswer}
+        allContentsForUser.push(content);
+    }
+
 }
 
 class DataView extends Component {
 
     constructor(props) {
         super(props);
+        generateFakeData();
     }
 
     render() {
