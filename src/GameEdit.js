4ed1c2c0a3490ca1093fc51409d2d3c2fe5fd794
Markus Strasser <markusstrasser@markuss-air.localdomain>
2018-07-11 16:00:51 +0200

    front end version of adding a custom game per URL

--- src/GameEdit.js
+++ src/GameEdit.js
@@ -4,16 +4,18 @@ import Button from '@material-ui/core/Button';
 import gameMetaData from "./gameMetaData"
 import globals from "./globals"
 import Tooltip from '@material-ui/core/Tooltip'
+import TextField from '@material-ui/core/TextField';
 
 class GameEdit extends Component {
     constructor({location}, props) {
         super();
-        // this.onChooseModule = this.onChooseModule.bind(this);
         this.onChooseGame = this.onChooseGame.bind(this);
-        this.updateEditor = this.updateEditor.bind(this);
+        this.submitNewGame = this.submitNewGame.bind(this);
         this.state = {
             chosen: !!props.value
         }
+        this.textInput = React.createRef();
+
     }
 
     componentWillReceiveProps(nextProps) {
@@ -22,21 +24,27 @@ class GameEdit extends Component {
         })
     }
 
-    onChooseGame(index) {
-        const game = gameMetaData[index];
+    onChooseGame(game) {
         this.setState({
             chosen: true
-        }, () => this.updateEditor(game, {type: globals.GAME}))
+        }, () => this.props.onChange(game, {type: globals.GAME}))
 
-    }
-
-    updateEditor(slide) {
-        console.log("SLLLIIIDEEM  ", slide)
-        this.props.onChange(slide);
     }
 
     handleTryOut() {
         window.open(`/games/${this.props.value.name}`);
+    }
+    submitNewGame() {
+        const url = this.textInput.current.value;
+        console.log(this.textInput);
+        const game = {
+            name: "testGame",
+            url: url,
+            primaryText: 'Test',
+            description: "test",
+            thumbnail: null
+        }
+        this.onChooseGame(game)
     }
 
     render() {
@@ -56,7 +64,20 @@ class GameEdit extends Component {
                         <Button variant="flat" color="primary" onClick={this.handleTryOut.bind(this)}> Try Out </Button>
                     </section>)
 
-                    : <ChooseGameGrid choose={this.onChooseGame}/>
+                    :
+                    (<div>
+                        {/*<TextField*/}
+                            {/*id="with-placeholder"*/}
+                            {/*label="Embed Simulation from URL"*/}
+                            {/*placeholder="Your URL goes here"*/}
+                            {/*margin="normal"*/}
+                            {/*ref={this.textInput}*/}
+                        {/*/>*/}
+                        <input ref={this.textInput} label="...add URL to embed your own Simulation" />
+                        <Button variant="flat" color="primary" onClick={this.submitNewGame}>Submit</Button>
+
+                        <ChooseGameGrid choose={this.onChooseGame}/>
+                    </div>)
                 }
             </div>
         )
@@ -74,9 +95,10 @@ const ChooseGameGrid = props => {
         width: width
     }
 
-    return (<section style={flexer}>
+    return (
+        <section style={flexer}>
             {gameMetaData.map((item, idx) =>
-                (<div key={idx} onClick={() => props.choose(idx)} style={{cursor: "pointer"}}>
+                (<div key={idx} onClick={() => props.choose(item)} style={{cursor: "pointer"}}>
                         <Tooltip fontSize={20} id="tooltip-fab" title={item.description}>
                             <img src={item.thumbnail} width={width / 3} style={{padding: 1 + "rem"}}/>
                         </Tooltip>
--- src/GameEmbedder.js
+++ src/GameEmbedder.js
@@ -6,9 +6,18 @@ import gameMetaData from "./gameMetaData"
 //location is otherwise thought of as Props....because first argument...
 
 const GameEmbedder = function ({location}) {
+    if (!location) {
+        location = window.location.href;
+    }
     const folders = location.pathname.split('/').filter(e => e.length > 0).slice(-2);
     const gameName = folders[1];
-    const url = gameMetaData.filter(d => (d.name === gameName))[0].url;
+    let url;
+    try {
+        url =gameMetaData.filter(d => (d.name === gameName))[0].url;
+    }
+    catch(error) {
+        alert("This Game is not yet added to the DataBase")
+    }
     return (
         <iframe style={{width: window.innerWidth, height: window.innerHeight * 0.85}}
                 allow="geolocation; microphone; camera;"
