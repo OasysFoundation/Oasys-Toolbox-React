e8e59da8ac5aab979221c27f3d0dae534b603e39
Markus Strasser <markusstrasser@markuss-air.localdomain>
2018-07-12 11:36:18 +0200

    make prev / next buttons more visible

--- src/ContentView.js
+++ src/ContentView.js
@@ -16,6 +16,15 @@ import { firebase } from './firebase';
 import HyperVideoEditor from './HyperVideoEditor';
 import Comment from './Comment'
 
+const buttonStyle = {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+};
 
 
 class ContentView extends Component {
@@ -223,15 +232,15 @@ class ContentView extends Component {
                     <MobileStepper
                       steps={content.data.length + 1}
                       activeStep={this.state.slideIdx}
-                      style = {{position: 'fixed', bottom: '0', width: '100%'}}
+                      style = {{position: 'fixed', bottom: '0', width: '100%', minHeight: 12 + "vh"}}
                       nextButton={
-                        <Button size="small" onClick={this.handleNext.bind(this)} disabled={this.state.slideIdx === content.data.length}>
+                        <Button size="large" style={this.state.slideIdx === content.data.length ? null : buttonStyle} onClick={this.handleNext.bind(this)} disabled={this.state.slideIdx === content.data.length}>
                           Next
                           {<KeyboardArrowRight />}
                         </Button>
                       }
                       backButton={
-                        <Button size="small" onClick={this.handlePrevious.bind(this)} disabled={this.state.slideIdx === 0}>
+                        <Button size="large" style={this.state.slideIdx === 0 ? null : buttonStyle} onClick={this.handlePrevious.bind(this)} disabled={this.state.slideIdx === 0}>
                           {<KeyboardArrowLeft />}
                           Back
                         </Button>
