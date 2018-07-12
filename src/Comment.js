ff93229db338074b85c6be18fc33c928bbf187bc
Frederik Riedel <frederik.riedel@frogg.io>
2018-07-12 12:41:18 +0200

    moved comment text box to the top so it's easier to write a new comment (instead of having to scroll down all the way) this fixes #131

--- src/Comment.js
+++ src/Comment.js
@@ -240,6 +240,13 @@ class CommentSection extends Component {
                         <Header as='h3' dividing>
                             Comments
                         </Header>
+
+                        <Form reply style={{marginBottom:'30px'}}>
+                            <Form.TextArea value={this.state.comment} onChange={this.addComment.bind(this)}/>
+                            <Button onClick={this.onSubmit.bind()} content='Add Comment' labelPosition='left' icon='edit'
+                                    primary/>
+                        </Form>
+
                         {this.state.comments.length == 0 ?
                             (null)
                             : (
@@ -247,11 +254,7 @@ class CommentSection extends Component {
                             )
                         }
 
-                        <Form reply>
-                            <Form.TextArea value={this.state.comment} onChange={this.addComment.bind(this)}/>
-                            <Button onClick={this.onSubmit.bind()} content='Add Reply' labelPosition='left' icon='edit'
-                                    primary/>
-                        </Form>
+                        
                     </Comment.Group>
                 </div>
             </section>
