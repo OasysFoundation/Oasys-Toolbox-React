6ef75ca319e6fe00229351eab1c23626fb8c9ffd
Frederik Riedel <frederik.riedel@frogg.io>
2018-07-13 15:45:57 +0200

    added test help tutorial video

--- src/Help.js
+++ src/Help.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import HelpIcon from '@material-ui/icons/Help';
+import Plyr from 'react-plyr';
 
 class Help extends Component {
 
@@ -16,7 +17,12 @@ class Help extends Component {
 	            <HelpIcon color="secondary" style={{ fontSize: '100px' }}/>
 	            <h1>Oasys Authoring Getting Started</h1> 
 	            </center>
-	            <div><p>We prepared videos and tutorials for you to help you get started with the Oasys Authoing Suite.</p></div>
+	            <div style={{margin:'40px'}}><p>We prepared videos and tutorials for you to help you get started with the Oasys Authoing Suite.</p></div>
+				<Plyr
+			      type="youtube"
+			      videoId="https://www.youtube.com/watch?v=9bUtZzi6hag"
+			      controls= {['play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen']}
+			    />
             </div>
             </center>
 			)
