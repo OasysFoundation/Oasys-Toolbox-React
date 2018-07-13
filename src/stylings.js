28313225d0f2aee2342b7cf5c6e5bbc29bc1b6d2
Markus Strasser <markusstrasser@markuss-air.localdomain>
2018-07-13 11:41:49 +0200

    quiz and gameedit have same style >> #136

--- src/stylings.js
+++ src/stylings.js
@@ -7,6 +7,7 @@ const buttonGradientCSS = {
         background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
         borderRadius: 3,
         border: 0,
+        margin: 0.5 + "rem",
         color: 'white',
         height: 48,
         padding: '0 25px',
