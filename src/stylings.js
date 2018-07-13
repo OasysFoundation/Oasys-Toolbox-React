87264716f5d3b78239cdc48cca9e0bd18ffc2bed
Markus Strasser <markusstrasser@markuss-air.localdomain>
2018-07-13 11:35:16 +0200

    preStyled Button in styling.js. Use to make UI look coherent

--- src/stylings.js
+++ src/stylings.js
@@ -1,11 +1,21 @@
+import React, {Component} from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
 const buttonGradientCSS = {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    borderRadius: 3,
-    border: 0,
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    coolPink: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 25px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)'
+    }
 };
 
-export {buttonGradientCSS}
+const ClassInject = props => <Button className={props.classes.coolPink}>{props.children ? props.children : 'class names'}</Button>
+
+const CoolPinkButton = withStyles(buttonGradientCSS)(ClassInject)
+
+export {buttonGradientCSS, CoolPinkButton}
