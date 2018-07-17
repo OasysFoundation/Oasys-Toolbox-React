5e7d49b2ef7b1ba259ec4851ece181b8d27d8f1c
Daniel Renz <daniel.l.renz@gmail.com>
2018-07-17 11:10:08 +0200

    added help/trend icons, started refactoring analytics

--- src/DataViewCreator.js
+++ src/DataViewCreator.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { firebase } from './firebase';
+import IconHelpOutline from '@material-ui/icons/HelpOutline';
+import IconTrendingUp from '@material-ui/icons/TrendingUp';
+import IconTrendingDown from '@material-ui/icons/TrendingDown';
 //import d3 from 'd3';
 import taucharts from 'taucharts';
 import 'taucharts/dist/plugins/tooltip';
@@ -12,179 +15,12 @@ import 'taucharts/dist/plugins/legend';
 import './taucharts.min.css';
 import { withStyles } from '@material-ui/core/styles';
 import d3 from 'd3';
+import ReactTooltip from "react-tooltip"
+import {summary} from './analytics/text'
+import {styles} from './analytics/styles'
+import {generateSlideTimes, generateQuizAnswers, generateSynthData} from './analytics/genSyntheticData'
+import {rearrangeData} from './analytics/processData'
 
-/*
-- aggregate analytics over contents to show how many users total accesses them.
-- add ability to sort contents by total users, last created, etc.
-- revamp quiz answer graph: show 0.5 and 1.0 in y axis.
-- comments per slide (NEED to talk with robbie about that)
-*/
-
-const styles = {
-  temp: {width: '1800px'},
-  pageWrap: {
-    marginTop: '20px',
-    marginLeft: '20px',
-  },
-  paperWrap:{
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    padding: 20,
-    width: '800px'
-  },
-  paper:{
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    margin: 20,
-    width: '800px'
-  },
-  paperSummary:{
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    margin: 20,
-    width: '1850px',
-    height: '330px',
-  },
-  paperElem: {
-    textAlign: 'center',
-    margin: 20,
-  },
-  quizWrap: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  quizElemCorrect: {
-    width: '10px',
-    height: '10px', 
-    backgroundColor: '#00aa00', 
-    border: '1px solid #dddddd',
-  },
-  quizElemWrong: {
-    width: '10px',
-    height: '10px', 
-    backgroundColor: '#aa0000', 
-    border: '1px solid #dddddd',
-  },
-  marginRight: {
-    marginRight: '5px',
-  },
-  barWrap: {
-    height: '250px',
-    width: '450px',
-  }
-};
-
-function generateFakeSlideTimes(n) {
-    let a = [];
-    let i=0;
-    while(true) {
-        a.push({i: i, t: Math.random()*60}); // max 1 minute per slide
-        if (i==n) { break; }
-        else if (Math.random() > 0.9 && i > 0) { i--; }
-        else {i++; }
-    }
-    if (Math.random() > 0.7) {
-        let maxnr = Math.round(Math.random()*a.length);
-        return a.slice(0,maxnr);
-    } else {
-        return a;
-    }
-}
-
-
-function generateFakeQuizAnswers(n) {
-    let a = [];
-    for (let i=0; i<n; i++) {
-        if (Math.random() > 0.7) {
-            a.push(false);
-        } else {
-            a.push(true);
-        }
-    }
-    return a;
-}
-
- function generateFakeData(){
-    let allContentsForUser = [];
-    for (let i=0;i<4;i++) {
-        let oneContentForUser = [];
-        let nSlides = 20 - Math.round(Math.random() * 10);
-        let nQuiz = 4 - Math.round(Math.random() * 3);
-        let nUsers = 25 - Math.round(Math.random() * 10);
-        for (let j=0;j<nUsers;j++) {
-            let startTime = new Date();
-            startTime.setTime(startTime.getTime() - Math.round(Math.random()*60*60*24*1000*30)); // startTime in interval [now-30 days, now]
-            let endTime = new Date();
-            endTime.setTime(startTime.getTime() + Math.round(Math.random()*60*24*1000)); 
-            let slideTimings = generateFakeSlideTimes(nSlides);
-            let quizAnswers = generateFakeQuizAnswers(nQuiz);
-            let contentId = "content " + (i + 1);
-            let duration = new Date();
-            duration.setTime(endTime.getTime() - startTime.getTime());
-            let content = {contentId: contentId, startTime: startTime, endTime: endTime, accessTimes: slideTimings, quizAnswers: quizAnswers, duration: duration}
-            oneContentForUser.push(content);
-        }
-        allContentsForUser.push(oneContentForUser);
-    }
-    return allContentsForUser;
-}
-
-function rearrangeData(contents) {
-    // find total number of slides. we should change this later by directly
-    // importing the content, and looking up number of slides
-    let nSlides = 0;
-    for (let i=0; i<contents.length; i++) {
-        nSlides = Math.max(nSlides, wrapTiming(contents[i].accessTimes).length);
-    }
-    let nQuiz = 0;
-    for (let i=0; i<contents.length; i++) {
-        nQuiz = Math.max(nQuiz, wrapTiming(contents[i].quizAnswers).length);
-    }
-
-    let usersPerSlide = Array.apply(null, Array(nSlides)).map(Number.prototype.valueOf,0);
-    let timingTemplate = Array.apply(null, Array(nSlides)).map(Number.prototype.valueOf,0);
-    let timings = []
-    for (let i=0; i<contents.length; i++) {
-        let timing = wrapTiming(contents[i].accessTimes).map(a => a.time);
-        let tt = timingTemplate.slice();
-        tt.splice(0, timing.length, ...timing);
-        timings.push(tt);
-        for (let j=0; j<timing.length; j++) {
-            usersPerSlide[j]++;
-        }
-    }
-    let timingsPerSlide = [...Array(nSlides)].map(e => Array(0));
-    for (let i=0; i<timings.length; i++) {
-        for (let j=0; j<timings[i].length; j++) {
-            timingsPerSlide[j].push(timings[i][j])
-        }
-    }
-    let answers = Array.apply(null, Array(nQuiz)).map(Number.prototype.valueOf,0);
-    for (let i=0; i<contents.length; i++) {
-        for (let j=0; j<contents[i].quizAnswers.length; j++) {
-            if (contents[i].quizAnswers[j]) {
-                // TODO: this is inccorectly divided by the number of users that accessed the content.
-                // instead, it should be divided by the number of users who answered this quiz question.
-                answers[j] += 1 / contents.length;
-            }
-        }
-    }
-    console.log(answers)
-    let data = {
-        usersPerSlide: usersPerSlide, 
-        timings: timings, 
-        nSlides: nSlides, 
-        timingsPerSlide: timingsPerSlide,
-        answers: answers,
-    }
-    return data
-}
 
 function padNumber(x) {
     return x.toString().padStart(2,'0')
@@ -208,7 +44,7 @@ class DataView extends Component {
         super(props);
         this.state = {
             data: null,
-            allContentsForUser: generateFakeData(),
+            allContentsForUser: generateSynthData(),
         }
         //this.loadContent();
     }
@@ -286,7 +122,10 @@ class DataView extends Component {
             },
             plugins: [
                 taucharts.api.plugins.get('tooltip')()
-            ]
+            ],
+            settings: {
+                fitModel: 'entire-view',
+            }
         });
         chart.renderTo(div);
     }
@@ -344,11 +183,7 @@ class DataView extends Component {
                             }
                     }]
                 }),
-                taucharts.api.plugins.get('legend')({
-                    settings: {
-                        position: 'bottom',
-                    }
-                }),
+                taucharts.api.plugins.get('legend')(),
             ]
         });
         chart.renderTo("#usersPerSlide"+idx);
@@ -411,7 +246,7 @@ class DataView extends Component {
             newDate.setDate(today.getDate() - 7*i);
             data.push({'week': newDate, 'comments': Math.round(6*Math.random())})
         }
-        this.renderLineChart(data, "week", "comments", "#commentsPerWeek", "number of comments");
+        this.renderLineChart(data, "week", "comments", "#commentsPerWeek", "comments");
     }
 
     componentDidMount(){
@@ -445,55 +280,62 @@ class DataView extends Component {
         */
     }
 
+    renderAnalyticsSummaryRow(obj, num){
+        return (
+            <tr>
+                <td>
+                    <Typography gutterBottom variant="body1">
+                        {obj.text} <IconHelpOutline style={styles.smallIcon} data-tip='tooltip' data-for={obj.id} />
+                        <ReactTooltip id={obj.id}> {obj.help} </ReactTooltip>
+                    </Typography>
+                </td>
+                <td style={styles.summaryTableCell}>
+                    <Typography gutterBottom variant="body1">
+                        {num}
+                        <IconTrendingUp style={styles.trendingUp} />
+                    </Typography>
+                </td>
+            </tr>
+        )
+    }
+
     render() {
         return (
             <div style={styles.pageWrap}>
-                <Typography gutterBottom variant="display1">
+                <Typography gutterBottom variant="title">
                     {"Overview"}
                 </Typography>
                     <Paper zDepth={3} style={styles.paperSummary}> 
                         <div style={styles.paperElem}>
-                            <Typography gutterBottom variant="headline">
+                            <Typography gutterBottom variant="subheading">
                                 {"Summary statistics"}
                             </Typography>
-                            <br/>
                             <div id="summaryWrap">
-                                <Typography gutterBottom variant="body1">
-                                    {"You have a total of " + this.state.allContentsForUser.length + " contents."}
-                                </Typography>
-                                <Typography gutterBottom variant="body1">
-                                    {"Total user feedback: " + 38 + " comments."}
-                                </Typography>
-                                <Typography gutterBottom variant="body1">
-                                    {"Average user rating: " + 3.8 + " stars."}
-                                </Typography>
-                                <Typography gutterBottom variant="body1">
-                                    {"You have earned " + 9.00 + " OAS tokens."}
-                                </Typography>
-                                <Typography gutterBottom variant="body1">
-                                    <strong>
-                                    {"Thus, you are the " + 4 + "th most valuable contributor - awesome!"}
-                                    </strong>
-                                </Typography>
+                                <table className="textAlignLeft">
+                                    {this.renderAnalyticsSummaryRow(summary.content, this.state.allContentsForUser.length)}
+                                    {this.renderAnalyticsSummaryRow(summary.comment, 38)}
+                                    {this.renderAnalyticsSummaryRow(summary.rating, 4.1)}
+                                    {this.renderAnalyticsSummaryRow(summary.tokens, 9.0)}
+                                </table>
                             </div>
                         </div>
                         <div id="usersPerWeekWrap" style={styles.paperElem}>
-                            <Typography gutterBottom variant="headline">
+                            <Typography gutterBottom variant="subheading">
                                 {"Users per week"}
                             </Typography>
-                            <div id={"usersPerWeek"} style={styles.barWrap}/>
+                            <div id={"usersPerWeek"} style={styles.graphWrap}/>
                         </div>
                         <div id="rewardsPerWeekWrap" style={styles.paperElem}>
-                            <Typography gutterBottom variant="headline">
+                            <Typography gutterBottom variant="subheading">
                                 {"Rewards per week"}
                             </Typography>
-                            <div id={"rewardsPerWeek"} style={styles.barWrap}/>
+                            <div id={"rewardsPerWeek"} style={styles.graphWrap}/>
                         </div>
                         <div id="commentsPerWeekWrap" style={styles.paperElem}>
-                            <Typography gutterBottom variant="headline">
+                            <Typography gutterBottom variant="subheading">
                                 {"Comments per week"}
                             </Typography>
-                            <div id={"commentsPerWeek"} style={styles.barWrap}/>
+                            <div id={"commentsPerWeek"} style={styles.graphWrap}/>
                         </div>
                     </Paper>
 
@@ -531,19 +373,19 @@ class DataView extends Component {
                             <Typography gutterBottom variant="headline">
                                 {"Users per week"}
                             </Typography>
-                            <div id={"usersPerWeek"+i} style={styles.barWrap}/>
+                            <div id={"usersPerWeek"+i} style={styles.graphWrap}/>
                         </div>
                         <div id="rewardsPerWeekWrap" style={styles.paperElem}>
                             <Typography gutterBottom variant="headline">
                                 {"Rewards per week"}
                             </Typography>
-                            <div id={"rewardsPerWeek"+i} style={styles.barWrap}/>
+                            <div id={"rewardsPerWeek"+i} style={styles.graphWrap}/>
                         </div>
                         <div id="commentsPerWeekWrap" style={styles.paperElem}>
                             <Typography gutterBottom variant="headline">
                                 {"Users/Comments per slide"}
                             </Typography>
-                            <div id={"usersPerSlide"+i} style={styles.barWrap}/>
+                            <div id={"usersPerSlide"+i} style={styles.graphWrap}/>
                         </div>
                     </Paper>
                  ))}
@@ -564,12 +406,12 @@ document.body.style.overflowX = 'auto';
     <Typography gutterBottom component="p">
         {"Average number of users per slide"}
     </Typography>
-    <div id={"usersPerSlide"+i} style={styles.barWrap}/>
+    <div id={"usersPerSlide"+i} style={styles.graphWrap}/>
 
     <Typography gutterBottom component="p">
         {"Average time spent on each slide"}
     </Typography>
-    <div id={"avgTime"+i} style={styles.barWrap}/>
+    <div id={"avgTime"+i} style={styles.graphWrap}/>
 
     <Typography gutterBottom component="p">
         {"Quiz answers"}
---
+++ src/analytics/text.js
@@ -0,0 +1,26 @@
+
+const summary = {
+    content: {
+        id: "contentSummaryHelp",
+        text: "Contents ",
+        help: "Total number of contents that you created.",
+    },
+    comment: {
+        id: "commentSummaryHelp",
+        text: "Comments ",
+        help: "Total number of comments from other users."
+    },
+    rating: {
+        id: "ratingSummaryHelp",
+        text: "User rating ",
+        help: "Average user rating of all your contents."
+    },
+    tokens: {
+        id: "tokensSummaryHelp",
+        text: "Tokens ",
+        help: "Total amount of tokens that you earned."
+    } 
+}
+
+
+export {summary};
