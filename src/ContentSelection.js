2749aafd9e618266b683db6f3ed887e1a6c3ff4e
Frederik Riedel <frederik.riedel@frogg.io>
2018-07-20 15:44:34 +0200

    added category filter: Featured / New / [Categories]. Fixes #191

--- src/ContentSelection.js
+++ src/ContentSelection.js
@@ -10,6 +10,9 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import MenuItem from '@material-ui/core/MenuItem';
 
 
 const Flexer = styled.section`
@@ -28,9 +31,11 @@ class ContentSelection extends Component {
         const that = this;
         this.state = {
             content: [],
+            filteredContent: [],
             searchText: '',
             searchAnchor: null,
             searchResults: [],
+            category: 'Featured'
             // isLoading: true
         }
 
@@ -41,7 +46,13 @@ class ContentSelection extends Component {
         })
         .then(function (myJson) {
             console.log(myJson);
-            that.setState({content: myJson});
+            that.setState({
+                content: myJson
+            }, function() {
+                that.setState({
+                    filteredContent: that.filteredContentForCategory(that.state.category)
+                })
+            });
 
         });
     }
@@ -70,6 +81,57 @@ class ContentSelection extends Component {
         this.setState({
             searchText: ''
         })
+    }
+
+    handleCategoryChange(event) {
+        this.setState({
+         category: event.target.value,
+         filteredContent: this.filteredContentForCategory(event.target.value)
+     });
+    }
+
+
+    filteredContentForCategory(category) {
+        if (category == 'Featured') {
+            return this.state.content;
+        }
+
+        if (category == 'Recently Added') {
+            return this.state.content;
+        }
+
+        var keywords = [];
+        if (category == 'Chemistry') {
+            keywords = ['chemistry', 'atom', 'molecule'];
+        }
+
+        if (category == 'Physics') {
+            keywords = ['physic', 'simulation', 'work'];   
+        }
+
+        if (category == 'Computer Science') {
+            keywords = ['object', 'programming', 'ml', 'swift', 'code'];
+        }
+
+        var that = this;
+        return this.state.content.filter(function(content) {
+            return that.stringContainsKeywords(content.tags, keywords);
+        })
+    }
+
+    stringContainsKeywords(string, keywords) {
+        var includesKeyword = false;
+        keywords.forEach(function(keyword) {
+            const lowercaseString = string.toLowerCase();
+            const lowercaseKeyword = keyword.toLowerCase();
+            if (lowercaseString.includes(lowercaseKeyword)) {
+                console.log(string);
+                includesKeyword = true;
+                return;
+            }
+        });
+
+        return includesKeyword;
     }
 
 
@@ -101,11 +163,30 @@ class ContentSelection extends Component {
         return (
             <div>
                 <Paper style={{margin:'16px', padding:'16px'}}>
+
+                    <FormControl style={{position:'absoulte', top:'0', left:'0'}}>
+                      <Select
+                        onChange={this.handleCategoryChange.bind(this)}
+                        displayEmpty
+                        value={this.state.category}
+                      >
+                        <MenuItem value="Featured">
+                          <em>Featured Content</em>
+                        </MenuItem>
+                        <MenuItem value="Recently Added">Recently Added</MenuItem>
+                        <MenuItem value="Physics">Physics</MenuItem>
+                        <MenuItem value="Chemistry">Chemistry</MenuItem>
+                        <MenuItem value="Computer Science">Computer Science</MenuItem>
+                      </Select>
+                    </FormControl>
+
                     <center>
                     <h2> Welcome to Oasys! </h2>
                     <p style={{maxWidth:'450px'}}>
                     Start your educational journey here. Search for topics you're interested in or select one of the personally curated contents below.
                     </p>
+
+                    
 
                     <TextField
                       id="search"
@@ -141,7 +222,7 @@ class ContentSelection extends Component {
                     {this.state.content.length===0? (
                             <CircularProgress style={{ color: 'orange' }} thickness={7} />
                         ) : (
-                            this.state.content.map((d, i) => <SimpleMediaCard key={i} contentData={d}/>)
+                            this.state.filteredContent.map((d, i) => <SimpleMediaCard key={i} contentData={d}/>)
                         )
                     }
                 </Flexer>
