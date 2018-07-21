bfeeb2b60a08d2946d46f09039ace66bb28b8779
Robbie Zuazua <rzuazua@utexas.edu>
2018-07-21 17:05:08 -0500

    first iteration of mobile navbar, still some bugs that i need to work out

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import ToolbarGroup from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button';
 import IconExplore from '@material-ui/icons/Explore';
 import IconCreate from '@material-ui/icons/Create';
@@ -19,17 +20,43 @@ import CloseIcon from '@material-ui/icons/Close';
 import {withStyles} from '@material-ui/core/styles';
 import ButtonBase from '@material-ui/core/ButtonBase';
 
+//import Navbar from '@coreui/react/lib';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+
 const styles = theme => ({
     close: {
         width: theme.spacing.unit * 4,
         height: theme.spacing.unit * 4,
     },
+    navbar: {
+        backgroundColor: "#74A4AC"
+    }
 });
 
 const BG = "#74A4AC";
 
 
 class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          isOpen: false
+        };
+    }
 
     state = {
         open: false,
@@ -47,12 +74,25 @@ class NavBar extends React.Component {
         this.setState({open: false});
     };
 
+    toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
     render() {
 
         const loggedIn = this.props.authUser && this.props.authUser !== "loggedOut";
 
         let accountMenuItems = (
             <Button href="/login" color="inherit">Sign In</Button>
+        );
+        let accountMenuItemsNewSecond = (null);
+
+        let accountMenuItemsNew = (
+                <NavItem style={{display: "flex",alignItems: "center", justifyContent: "center"}}>
+                    <NavLink href="/login/" className={"text-white"} style={{flex: 1}}>Sign In</NavLink>
+                </NavItem>
         );
 
         if (loggedIn) {
@@ -67,6 +107,20 @@ class NavBar extends React.Component {
                 </div>
             )
         }
+
+        if (loggedIn) {
+            accountMenuItemsNew = (
+                <NavItem style={{display: "flex",alignItems: "center", justifyContent: "center"}}>
+                    <IconAccountCircle style={{marginRight: '7px', color:"white", flex: 1}}/>
+                    <NavLink href="/user/" className={"text-white"} style={{flex: 1}}>{this.props.authUser.displayName}</NavLink>
+                </NavItem>
+            );
+            accountMenuItemsNewSecond = (
+                <SignOutButton color="inherit" handleClick={this.handleClick.bind(this)}/>
+            );
+
+        }
+
 
         let navBarElements = (
             <div>
@@ -92,35 +146,44 @@ class NavBar extends React.Component {
             </div>
         );
 
+        let navBarElementsNew = (
+            <Nav className="ml-auto" navbar>
+              <NavItem style={{display: "flex",alignItems: "center", justifyContent: "center"}}>
+                <IconExplore style={{marginRight: '7px', color:"white", flex: 1}}/>
+                <NavLink href="/explore/" className={"text-white"} style={{flex: 1}}>Explore</NavLink>
+              </NavItem>
+              <NavItem style={{display: "flex",alignItems: "center", justifyContent: "center"}}> 
+                <IconCreate style={{marginRight: '7px',color:"white", flex: 1}}/>
+                <NavLink href="/create/" className={"text-white"} style={{flex: 1}} >Create</NavLink>
+              </NavItem>
+              <NavItem style={{display: "flex",alignItems: "center", justifyContent: "center"}}>
+                <IconInsertChart style={{marginRight: '7px', color:"white", flex: 1}}/>
+                <NavLink href="/data/" className={"text-white"} style={{flex: 1}}>Analytics</NavLink>
+              </NavItem>
+              {accountMenuItemsNew}
+              {accountMenuItemsNewSecond}
+
+              <NavItem style={{display: "flex",alignItems: "center", justifyContent: "center"}}>
+                <NavLink href='https://joinoasys.org' className={"text-white"}>About</NavLink>
+              </NavItem>
+            </Nav>
+        );
+
 
         const {classes} = this.props;
         return (
-            <AppBar position="static">
-                <section
-                    style={{backgroundColor: BG, display: "flex", flexDirection: "row", justifyContent: "flex-end"}}>
-                    <div style={{textAlign: "start", flexGrow: 1, padding: "1em", display: 'inline-block'}}>
-                        <ButtonBase
-                            onClick={function (event) {
-                                event.preventDefault();
-                                window.location.href = '/explore/'
-                            }} style={{textAlign: "start", flexGrow: 1, padding: "1em", display: 'inline-block'}}>
-                            <Typography variant="title" color="inherit" href='/explore'>
-                                Oasys Education
-                            </Typography>
-                        </ButtonBase>
-                    </div>
+        <div>
 
-                    <Toolbar style={{display: "flex", justifyContent: "flex-end"}}>
-                        <div>
-                            {this.props.authUser
-                                ? navBarElements
-                                : null
-                            }
-                        </div>
-                    </Toolbar>
-                </section>
-
-                <Snackbar
+        <Navbar light style={{backgroundColor: BG}} expand="md">
+          <NavbarBrand href="/explore" className={"text-white"} style={{padding: "1em"}}>Oasys Education</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            {this.props.authUser
+                ? navBarElementsNew
+                : null
+            }
+          </Collapse>
+          <Snackbar
                     anchorOrigin={{
                         vertical: 'bottom',
                         horizontal: 'left',
@@ -145,7 +208,10 @@ class NavBar extends React.Component {
                     ]}
                 />
 
-            </AppBar>
+        </Navbar>
+
+            
+            </div>
 
         )
     }
--- src/SignOutButton.js
+++ src/SignOutButton.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import { auth } from './firebase';
 import Button from '@material-ui/core/Button';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
 class SignOutButton extends React.Component {
 
@@ -12,11 +24,17 @@ class SignOutButton extends React.Component {
 
    render() {
     return (
-      <Button
+
+      <NavItem style={{display: "flex",alignItems: "center", justifyContent: "center"}}>
+                    
+      <Button style={{marginRight: '7px', color:"white", flex: 1}}
         type="button"
         color="inherit"
         onClick={this.handleClick.bind(this)}
       >Sign Out</Button>
+
+    </NavItem>
+
     )
   }
 }
