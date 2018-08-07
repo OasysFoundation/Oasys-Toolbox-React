999763333ac8c6eb3d79886933834fce44414f83
Markus Strasser <strasser.ms@gmail.com>
2018-08-07 15:29:50 +0200

    ..

--- src/globals.js
+++ src/globals.js
@@ -4,7 +4,9 @@ const globals = {
 	//change if somebody committed with the wrong setting!!
 	//.env ENV_NODE
     // HARDSET_BASE: "http://localhost:8080/",
-    HARDSET_BASE: "https://api.joinoasys.org/",
+    ICON_FONTSIZE_MIDDLE: "20px",
+
+	HARDSET_BASE: "https://api.joinoasys.org/",
 
 
     API_DEV_LOCAL: 'http://localhost:8080/',
