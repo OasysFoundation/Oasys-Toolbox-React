994ab81cf0ec4ff141b1dbf2de4a93fb437da546
Daniel Renz <daniel.l.renz@gmail.com>
2018-08-08 21:28:35 +0200

    toc finished (for now -)

--- src/LessonMaker/AppSidebarToc.js
+++ src/LessonMaker/AppSidebarToc.js
@@ -4,41 +4,54 @@ import * as tocjs from '../assets/scripts/toc.js'
 
 class AppSidebarToc extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            height: 500,
+            width: 300,
+        };
+    }
+
+    handleChangeChapter(num){
+        console.log("change to chapter " + num);
+    }
+
     componentDidMount(){
         const opt = {
             tocId: 'toc',
-            totalWidth: 300,
+            totalWidth: this.state.width,
             rectHeight: 35,
             gapx: 5,
-            gapy: 10,
+            gapy: 15,
             arrowOffset: 0.04, // in percent of total width
-            //arrowColor: '#F8F8F4',
-            arrowColor: '#aa0000',
+            arrowColor: '#C3C8D4',
+            arrowLongColor: '#F4EFB6',
+            arrowColorReverse: '#A47D90',
+            arrowLongColorReverse: '#a34079', //A34079
             arrowStroke: 4,
-            backgroundColor: '#ffffff',
+            backgroundColor: '#2f353a',
             rectColorStart: '#3f51d5',
             rectColorEnd: '#3f51d5',
             rectColorDefaultFill: '#30444D',
             rectColorDefaultStroke: '#626970',
             textColor: '#eeeeee',
             myOrange: '#C85C0D',
+            handler: this.handleChangeChapter,
         }
 
         let chapters = [
-            {idx: 0, title: 'Chapter 1: Wow me introduction', links: [1,3]},
-            {idx: 1, title: 'Chapter 2: How to Wow', links: [2,3]},
-            {idx: 2, title: 'Chapter 3a: Text', links: [4]},
-            {idx: 3, title: 'Chapter 3b: Sim', links: [4,0]},
-            {idx: 4, title: 'Chapter 4: Interlude', links: [5]},
-            {idx: 5, title: 'Chapter 5: In depth wowing', links: [4,6,7,8,9,10,11]},
-            {idx: 6, title: 'Chapter6a', links: [13]},
-            {idx: 7, title: 'Chapter6b', links: [13]},
-            {idx: 8, title: 'Chapter6c', links: [13]},
-            {idx: 9, title: 'Chapter6d', links: [12]},
-            {idx: 10, title: 'Chapter6e', links: [13]},
-            {idx: 11, title: 'Chapter6f', links: [13]},
-            {idx: 12, title: 'Secret', links: [5]},
-            {idx: 13, title: 'Final test', links: []},
+            {idx: 0, title: 'Chapter 1: Wow me introduction', linkIdx: [1,3]},
+            {idx: 1, title: 'Chapter 2: How to Wow', linkIdx: [2,3]},
+            {idx: 2, title: 'Chapter 3a: Text', linkIdx: [4]},
+            {idx: 3, title: 'Chapter 3b: Sim', linkIdx: [4,0]},
+            {idx: 4, title: 'Chapter 4: Interlude', linkIdx: [5]},
+            {idx: 5, title: 'Chapter 5: In depth wowing', linkIdx: [4,6,7,8,9,10,11]},
+            {idx: 6, title: 'Chapter6a', linkIdx: [11]},
+            {idx: 7, title: 'Chapter6b', linkIdx: [11]},
+            {idx: 8, title: 'Chapter6c', linkIdx: [11]},
+            {idx: 9, title: 'Chapter6e', linkIdx: [11]},
+            {idx: 10, title: 'Chapter6f', linkIdx: [11]},
+            {idx: 11, title: 'Final test', linkIdx: []},
         ];
 
         // let nColors = chapters.map(e=>e.links.length).reduce((a,c)=>a+c);
@@ -58,8 +71,8 @@ class AppSidebarToc extends Component {
 
     render() {
         return (
-            <svg xmlns="http://www.w3.org/2000/svg">
-                <svg id="toc" width="300" height="800" viewBox="0 0 300 800">
+            <svg xmlns="http://www.w3.org/2000/svg" width={this.state.width} height={this.state.height} viewBox={"0 0 this.state.width this.state.height"}>
+                <svg id="toc" width={this.state.width} height={this.state.height} viewBox={"0 0 this.state.width this.state.height"}>
                 </svg>
             </svg>
         );
--- src/assets/scripts/toc.js
+++ src/assets/scripts/toc.js
@@ -11,12 +11,12 @@ export function longestPath(graph) {
     let pathCosts = [];
     function findAllPaths(idx, currentCost, currentPath) {
         currentPath.push(idx);
-        if (graph[idx].links.length===0) {
+        if (graph[idx].linkIdx.length===0) {
             pathCosts.push(currentCost);
             paths.push(currentPath);
         }
-        for (let i = 0; i < graph[idx].links.length; i++) {
-            let child = graph[idx].links[i];
+        for (let i = 0; i < graph[idx].linkIdx.length; i++) {
+            let child = graph[idx].linkIdx[i];
             if (child > idx) {
                 findAllPaths(child, currentCost+1, currentPath.slice());
             }
@@ -32,10 +32,10 @@ export function prepareToc(mainPath, chapters) {
         tocInfo.push({
             idx: mainPath[i],
             level: i,
-            next: chapters[mainPath[i]].links.filter(e=>e>=mainPath[i]),
-            prev: chapters[mainPath[i]].links.filter(e=>e<mainPath[i]),
+            next: chapters[mainPath[i]].linkIdx.filter(e=>e>=mainPath[i]),
+            prev: chapters[mainPath[i]].linkIdx.filter(e=>e<mainPath[i]),
             title: chapters[mainPath[i]].title,
-            links: chapters[mainPath[i]].links,
+            linkIdx: chapters[mainPath[i]].linkIdx,
         });
     }
     return tocInfo;
@@ -48,13 +48,13 @@ export function sortIntoTocLevels(tocInfo, chapters, mainPath) {
     for (let i=0;i<todo.length;i++) {
         let idx = chapters[todo[i]].idx;
         // find element pointing to this one
-        let chaps = chapters.filter(e=>e.links.indexOf(idx)>=0);
+        let chaps = chapters.filter(e=>e.linkIdx.indexOf(idx)>=0);
         if (chaps.length===0) {
             throw new Error("While building TOC, a chapter was discovered that is disconnected (" + chapters[todo[i]].title + ")")
         }
         let prevElem = Math.max(0,...chaps.map(e=>e.idx).filter(e=>e<idx));
         // find first element this one points to
-        let nextElem = Math.min(...chapters[idx].links.filter(e=>e>idx));
+        let nextElem = Math.min(...chapters[idx].linkIdx.filter(e=>e>idx));
         // how many possible levels?
         let posrefs = tocInfo.map(e=>e.idx).filter(e=>e>prevElem&&e<nextElem);
         let poslevs = tocInfo.filter(e=>posrefs.indexOf(e.idx) >= 0).map(e=>e.level);
@@ -66,15 +66,15 @@ export function sortIntoTocLevels(tocInfo, chapters, mainPath) {
         addTocInfo.push({
             idx: idx,
             level: poslevs[0],
-            next: chapters[idx].links.filter(e=>e>idx),
-            prev: chapters[idx].links.filter(e=>e<idx),
+            next: chapters[idx].linkIdx.filter(e=>e>idx),
+            prev: chapters[idx].linkIdx.filter(e=>e<idx),
             title: chapters[idx].title,
-            links: chapters[idx].links,
+            linkIdx: chapters[idx].linkIdx,
         });
     }
     tocInfo.push(...addTocInfo);
     // find final element and make sure it is at the highest level
-    let fin = tocInfo.filter(e=>e.links.length===0);
+    let fin = tocInfo.filter(e=>e.linkIdx.length===0);
     if (fin.length===0 || fin.length>1) {
         throw new Error("While building TOC, more then one final chapter was discovered.");
     }
@@ -113,7 +113,7 @@ export function reorderX(tocInfo) {
             // let myx = allonlvl.map(e => e.x);
             for (let k=0; k<allonlvl.length; k++) {
                 let elem = allonlvl[k];
-                let otherElems = tocInfo.filter(e => e.links.indexOf(elem.idx) >= 0);
+                let otherElems = tocInfo.filter(e => e.linkIdx.indexOf(elem.idx) >= 0);
                 let otherXmin = otherElems.map(e => e.x / e.nElems);
                 let otherXmax = otherElems.map(e => (e.x+1) / e.nElems);
                 elem.xmin = elem.x / elem.nElems;
@@ -145,8 +145,8 @@ export function insertArrowLocs(tocInfo, opt) {
     for (let i=0; i<tocInfo.length; i++) {
         let elem = tocInfo[i];
         elem.xarrow = [];
-        for (let j=0; j<elem.links.length; j++) {
-            let link = tocInfo[tocIdx.indexOf(elem.links[j])];
+        for (let j=0; j<elem.linkIdx.length; j++) {
+            let link = tocInfo[tocIdx.indexOf(elem.linkIdx[j])];
             let loc = (Math.min(link.xmax,elem.xmax) + Math.max(link.xmin,elem.xmin)) / 2;
             // console.log("[" + link.xmin + "," + link.xmax + "]; " + "[" + elem.xmin + "," + elem.xmax + "]" + "-->" + loc)
             for (let k=Math.min(elem.level, link.level); k<Math.max(elem.level, link.level); k++) {
@@ -183,16 +183,16 @@ export function drawChapters(tocInfo, chapters, opt) {
         let elems = tocInfo.filter(e=>e.level === i);
         if (elems.length === 1) {
             let myrect = {x: offx, y: offy, height: opt.rectHeight, width: opt.totalWidth, colorFill: opt.rectColorDefaultFill, colorStroke: opt.rectColorDefaultStroke};
-            document.getElementById(opt.tocId).appendChild(svgRect(myrect));
+            document.getElementById(opt.tocId).appendChild(svgRect(myrect,elems[0].idx,opt.handler));
             let mytext = {x: offx+10, y: offy+23, text: chapters[elems[0].idx].title, color: opt.textColor};
-            document.getElementById(opt.tocId).appendChild(svgText(mytext));
+            document.getElementById(opt.tocId).appendChild(svgText(mytext,elems[0].idx,opt.handler));
         } else {
             let rectWidth = Math.floor((opt.totalWidth - (elems.length - 1) * opt.gapx) / elems.length);
             for (let j=0; j<elems.length; j++) {
                 let myrect = {x: offx, y: offy, height: opt.rectHeight, width: rectWidth, colorFill: opt.rectColorDefaultFill, colorStroke: opt.rectColorDefaultStroke};
                 let mytext = {x: offx+10, y: offy+23, text: chapters[elems[j].idx].title, color: opt.textColor};
-                document.getElementById(opt.tocId).appendChild(svgRect(myrect));
-                document.getElementById(opt.tocId).appendChild(svgText(mytext));
+                document.getElementById(opt.tocId).appendChild(svgRect(myrect,elems[j].idx,opt.handler));
+                document.getElementById(opt.tocId).appendChild(svgText(mytext,elems[0].idx,opt.handler));
                 offx = offx + rectWidth + opt.gapx;
             }
         }
@@ -201,8 +201,9 @@ export function drawChapters(tocInfo, chapters, opt) {
 }
 
 export function drawConnections(tocInfo, opt){
+    let arrowToDraw = [];
     for (let i=0; i<tocInfo.length; i++) {
-        let links = tocInfo[i].links;
+        let links = tocInfo[i].linkIdx;
         for (let j=0; j<links.length; j++) {
             let elem = tocInfo.filter(e=>e.idx === links[j])[0];
             let y1 = tocInfo[i].level * (opt.gapy + opt.rectHeight);
@@ -215,26 +216,25 @@ export function drawConnections(tocInfo, opt){
                 y1 += opt.rectHeight + 1;
                 y2 -= 1;
             }
-            drawArrow(x,y1,y2,opt);
             let level1 = Math.min(tocInfo[i].level,elem.level);
             let level2 = Math.max(tocInfo[i].level,elem.level);
             for (let k=1; k<level2-level1; k++) {
                 drawTunnel(level1+k,x,opt);
             }
+            arrowToDraw.push({x:x,y1:y1,y2:y2});
         }
     }
+    arrowToDraw.forEach(e=>drawArrow(e.x,e.y1,e.y2,opt));
 }
 
 export function drawTunnel(level,x,opt) {
     let y1 = level * (opt.rectHeight + opt.gapy);
     let y2 = y1 + opt.rectHeight;
-    console.log(y1)
-    console.log(y2)
-    document.getElementById(opt.tocId).appendChild(svgBezier(x-5,x+5,y1-6,y1+1,opt.arrowColor));
-    document.getElementById(opt.tocId).appendChild(svgBezier(x-5,x+5,y2-6,y2+1,opt.backgroundColor));
+    document.getElementById(opt.tocId).appendChild(svgBezier(x-6,x+6,y1-7,y1+1,opt.rectColorDefaultFill,opt.rectColorDefaultStroke));
+    document.getElementById(opt.tocId).appendChild(svgBezier(x-6,x+6,y2-7,y2+1,opt.backgroundColor,opt.rectColorDefaultStroke));
 }
 
-export function svgBezier(x1,x2,y1,y2,color) {
+export function svgBezier(x1,x2,y1,y2,colorFill,colorStroke) {
     // play with beziers: http://blogs.sitepointstatic.com/examples/tech/svg-curves/cubic-curve.html
     // the path command consists of the starting point specified by M followed by x and y coords
     // the C starts a bezier curve, which consists of three additional points (and their respective x and y coords): 
@@ -245,36 +245,43 @@ export function svgBezier(x1,x2,y1,y2,color) {
     //poly.setAttribute('points', `${x1},${y1} ${x},${y2}, ${x2},${y1}`);
     let elem = document.createElementNS(NS, 'path');
     elem.setAttribute('d', `M${x1},${y2} C${x1},${y1} ${x2},${y1} ${x2},${y2}`);
-    elem.setAttribute('fill', color);
+    elem.setAttribute('fill', colorFill);
+    elem.setAttribute('stroke', colorStroke);
     return elem;
 }
 
-export function svgPoly(x,y1,y2,opt) {
-    let poly = document.createElementNS(NS, 'polygon');
-    poly.setAttribute('fill', opt.arrowColor);
-    poly.setAttribute('points', `${x-5},${y1} ${x},${y2}, ${x+5},${y1}`);
-    return poly;
-}
 
 export function drawArrow(x,y1,y2,opt) {
+    let reverse = false;
     if (y1<y2) {
-        document.getElementById(opt.tocId).appendChild(svgArrow(x, y1-5, y2, opt));
-        document.getElementById(opt.tocId).appendChild(svgPoly(x, y2-3, y2+8, opt));
+        let color = opt.arrowColor;
+        if (Math.abs(y2-y1)>opt.rectHeight) {
+            color = opt.arrowLongColor;
+        }
+        document.getElementById(opt.tocId).appendChild(svgArrow(x, y1-3, y2, color, reverse, opt));
+        document.getElementById(opt.tocId).appendChild(svgPoly(x, y2-6, y2+4, color, reverse, opt));
     } else {
-        document.getElementById(opt.tocId).appendChild(svgArrow(x, y2-5, y1, opt));
-        document.getElementById(opt.tocId).appendChild(svgPoly(x, y1-3, y1+8, opt));
+        reverse = true; 
+        let color = opt.arrowColorReverse;
+        if (Math.abs(y2-y1)>opt.rectHeight) {
+            color = opt.arrowLongColorReverse;
+        }
+        document.getElementById(opt.tocId).appendChild(svgArrow(x, y1+3, y2, color, reverse, opt));
+        document.getElementById(opt.tocId).appendChild(svgPoly(x, y2-6, y2+4, color, reverse, opt));
     }
 }
 
-export function svgRect(obj){
+export function svgRect(obj,idx,handler){
     let svg = document.createElementNS(NS,"rect");
     svg.width.baseVal.value=obj.width;
     svg.height.baseVal.value=obj.height;
     svg.setAttribute("x", obj.x);
     svg.setAttribute("y", obj.y);
     svg.setAttribute("opacity", 1.0);
-    svg.style.fill=obj.colorFill;
-    svg.style.stroke=obj.colorStroke;
+    svg.addEventListener("click", function(){handler(idx)}, false);
+    svg.style.fill = obj.colorFill;
+    svg.style.stroke = obj.colorStroke;
+    svg.style.cursor = 'pointer';
     /*if (idx===0) {
         svg.style.fill=opt.rectColorStart;
     } else if (idx===chapters.length-1) {
@@ -285,26 +292,47 @@ export function svgRect(obj){
     return svg;
 }
 
-export function svgText(obj) {
+export function svgText(obj,idx,handler) {
     let txt = document.createElementNS(NS, 'text');
     txt.setAttribute('x', obj.x);
     txt.setAttribute('y', obj.y);
     txt.setAttribute('fill', obj.color);
     txt.setAttribute('font-family', 'Arial');
     txt.setAttribute('font-size', '16');
+    txt.addEventListener("click", function(){handler(idx)}, false);
+    txt.style.cursor = 'pointer';
     txt.textContent = obj.text;
     return txt;
 }
 
-export function svgArrow(x,y1,y2,opt) {
+export function svgPoly(x,y1,y2,color,reverse,opt) {
+    let poly = document.createElementNS(NS, 'polygon');
+    poly.setAttribute('fill', color);
+    if (reverse) {
+        poly.setAttribute('points', `${x-5},${y2} ${x},${y1}, ${x+5},${y2}`);
+    } else {
+        poly.setAttribute('points', `${x-5},${y1} ${x},${y2}, ${x+5},${y1}`);
+    }
+    return poly;
+}
+
+export function svgArrow(x,y1,y2,color,reverse,opt) {
     let line = document.createElementNS(NS, 'line');
     line.setAttribute('x1',x);
     line.setAttribute('x2',x);
     line.setAttribute('y1',y1);
     line.setAttribute('y2',y2);
-    line.setAttribute('stroke',opt.arrowColor);
+    line.setAttribute('stroke',color);
     line.setAttribute('stroke-width',opt.arrowStroke);
-    line.setAttribute('stroke-dasharray', '10 35');
+    if (reverse) {
+        let dashDraw = opt.gapy+4;
+        let dashGap  = opt.rectHeight+5;
+        line.setAttribute('stroke-dasharray', dashDraw.toString() + ' ' + dashGap.toString());
+    } else {
+        let dashDraw = opt.gapy-5;
+        let dashGap  = opt.rectHeight+5;
+        line.setAttribute('stroke-dasharray', dashDraw.toString() + ' ' + dashGap.toString());
+    }
     // poly.setAttribute('stroke', colorStroke);
     return line;
 }
