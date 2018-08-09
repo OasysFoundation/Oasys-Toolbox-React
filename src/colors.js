de41881eeefbb10d5a18eedd4755093f81282a67
Frederik Riedel <frederik.riedel@frogg.io>
2018-08-09 10:59:23 +0200

    started to have colors in their own file

---
+++ src/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+	SNOW1: "#F8F8F4",
+	SNOW2: "#F4F4E8",
+	WINTERSUN: "#F4EFB6",
+	LOCHINVAR: "#2A9699",
+	SUMMERSUN: "#F6BD4A",
+	VELVET: "#C6361D",
+	GREEN: "#​​​​​​​2FC46D",
+	BLUESTEEL: "#​​​​​​​27363E",
+	PURPLE: "#​​​​​​​A34079",
+};
+
+export default colors;
