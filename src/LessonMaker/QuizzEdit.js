1ccd701029f97d3775de5f85dad21a90394f18c3
Markus Strasser <strasser.ms@gmail.com>
2018-08-17 14:28:40 +0200

    sidebartoc draws correctly, store update on quiz

--- src/LessonMaker/QuizzEdit.js
+++ src/LessonMaker/QuizzEdit.js
@@ -56,7 +56,6 @@ class QuizzEdit extends Component {
                 answers: that.state.answers,
                 quizType: that.state.quizType
             }, true)
-            that.props.updateChapterLinks();
         });
     }
 
