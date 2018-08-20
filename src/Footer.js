1eb8233aeb60ae6fbe44d346e8fcf45c37781e22
Daniel Renz <daniel.l.renz@gmail.com>
2018-08-20 12:15:21 +0200

    fixed many issues with navbar/footer style. updated logo

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 class FooterPage extends React.Component {
     render(){
         return(
-            <Footer color="blue-grey" className="page-footer font-small lighten-5 pt-1 mb-0 pb-3" style={{backgroundColor:"#f8f8f4"}}>
+            <Footer color="blue-grey" className="page-footer font-small lighten-5 pt-4" style={{backgroundColor:"#f8f8f4"}}>
                 <Container className="mt-5 text-center text-md-left">
                     <Row className="mt-3" style={{display:"flex", justifyContent:"center"}}>
                         <Col md="3" lg="4" xl="3" className="mb-4 dark-grey-text">
