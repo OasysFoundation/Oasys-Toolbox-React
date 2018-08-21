9657af644aa21f8ed8d804df0d9fb0599a85e59a
Robbie Zuazua <rzuazua@utexas.edu>
2018-08-21 10:29:30 -0500

    footer border

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 class FooterPage extends React.Component {
     render(){
         return(
-            <Footer color="blue-grey" className="page-footer font-small lighten-5 pt-4" style={{backgroundColor:"#f8f8f4"}}>
+            <Footer color="blue-grey" className="page-footer font-small lighten-5 pt-4" style={{backgroundColor:"#f8f8f4", borderTop:"1px solid #27363E"}}>
                 <Container className="mt-5 text-center text-md-left">
                     <Row className="mt-3" style={{display:"flex", justifyContent:"center"}}>
                         <Col md="3" lg="4" xl="3" className="mb-4 dark-grey-text">
