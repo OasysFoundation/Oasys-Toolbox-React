dc2cb12fca05460a26a3cc30f2dffdea009bf40e
Frederik Riedel <frederik.riedel@frogg.io>
2018-08-28 16:57:59 +0200

    implementing ContentTileMenuModal

---
+++ src/ExploreContent/ContentTileMenuModal.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { Button, Modal, ModalBody, ModalHeader } from 'reactstrap';
+
+const styles = {
+    mobileTopPadding:{
+        paddingTop:"100px"
+    },
+    pcTopPadding:{
+        paddingTop:"50px"
+    },
+    modalOuterDiv:{
+        fontFamily:"Raleway-Regular",
+    },
+    modalHeader:{
+        fontSize:"2.5em"
+    },
+    modalBody:{
+        display: "flex",
+        flexDirection: "column",
+        fontSize:"1.5em",
+    },
+    modalButton:{
+        display:"flex",
+        padding:"1em",
+    },
+}
+
+
+//this is the new "Preview" Component
+class ContentTileMenuModal extends Component {
+
+
+    toggle() {
+
+    }
+
+    handleClick() {
+
+    }
+
+
+    render() {
+    	
+        return (
+            <div>
+              <Modal isOpen={this.props.isOpen} toggle={this.toggle.bind(this)}
+                           className={'modal-sm ' + this.props.className} style={styles.modalOuterDiv}>
+              <ModalHeader toggle={this.toggle.bind(this)} style={styles.modalHeader}>{this.props.data.title}</ModalHeader>
+              <ModalBody style={styles.modalBody}>
+                  <Button block color="light" onClick={this.handleClick.bind(this,"remix")} style={styles.modalButton}>
+                    <div style={{flex:1}}><i className="fas fa-pencil-alt"/></div>
+                    <div style={{flex:3, textAlign:"left"}}>Remix</div>
+                  </Button>
+                  <Button block color="light" onClick={this.handleClick.bind(this,"comments")} style={styles.modalButton}>
+                    <div style={{flex:1}}><i className="fas fa-comment" /></div>
+                    <div style={{flex:3, textAlign:"left"}}>View Comments</div>
+                  </Button>
+                  <Button block color="light" onClick={this.handleClick.bind(this,"user")} style={styles.modalButton}>
+                    <div style={{flex:1}}><i className="fas fa-user"/></div>
+                    <div style={{flex:3, textAlign:"left"}}>{"Go To " + this.props.data.username + "'s Page"}</div>
+                  </Button>
+                  <Button block color="light" onClick={this.handleClick.bind(this,"collection")} style={styles.modalButton}>
+                    <div style={{flex:1}}><i className="fas fa-layer-group"/></div>
+                    <div style={{flex:3, textAlign:"left"}}>Create New Collection</div>
+                  </Button>
+                  <Button block color="light" onClick={this.handleClick.bind(this,"flag")} style={styles.modalButton}>
+                    <div style={{flex:1}}><i className="fas fa-flag"/></div>
+                    <div style={{flex:3, textAlign:"left"}}>Flag as Inappropriate</div>
+                  </Button>
+
+              </ModalBody>
+              </Modal>
+            </div>
+        )
+    }
+}
+
+ContentTileMenuModal.propTypes = {
+
+}
+
+export default ContentTileMenuModal;
